bf7bf5cbfa68458bcf262439e9fcbfd9ad41d5fa
Leaf <leaf-lsgtky@github.com>
2026-06-04 23:17:42 +0800

    fix: enable edge-to-edge for blur effect to work

--- manager/src/main/java/moe/shizuku/manager/home/HomeActivity.kt
+++ manager/src/main/java/moe/shizuku/manager/home/HomeActivity.kt
@@ -4,7 +4,9 @@ import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
@@ -38,6 +40,18 @@ open class HomeActivity : AppActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            ),
+        )
+        window.isNavigationBarContrastEnforced = false
 
         setContent {
             ShizukuAppTheme {
